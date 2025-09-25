68954d12b1212ee82a8b207ea2886459fa7994e1
Zack Tanner <1939140+ztanner@users.noreply.github.com>
2025-09-24 19:05:47 -0700

    [breaking]: enable clientSegmentCache

--- packages/next/src/server/config-shared.ts
+++ packages/next/src/server/config-shared.ts
@@ -1440,7 +1440,7 @@ export const defaultConfig = Object.freeze({
     serverSourceMaps: false,
     linkNoTouchStart: false,
     caseSensitiveRoutes: false,
-    clientSegmentCache: false,
+    clientSegmentCache: true,
     rdcForNavigations: false,
     clientParamParsing: false,
     clientParamParsingOrigins: undefined,
